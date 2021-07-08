2f86f00d5f65ca6a3f3957583121f16d12fd752b
Timplayer <tim.jae@gmx.de>
2021-07-08 17:29:29 +0200

    Remove Led Lib

--- Motor Steuerung/Roboter/Roboter/src/main.cpp
+++ Motor Steuerung/Roboter/Roboter/src/main.cpp
@@ -1,21 +1,12 @@
 #include"utility/Vector2.h"
 #include"Roboter/Roboter.h"
 #include"Lidar/Lidar.h"
-#include"Led/lib/ws2812-rpi.h"
 #include"iostream"
 #include<cstdio>
 
 int main(int argc, char** argv) {
 
-    std::cout << "test" << std::endl;
-    NeoPixel* n = new NeoPixel(24);
-    std::cout << "Test2" << std::endl;
-    while (true) {
-        std::cout << "Test" << std::endl;
-        n->rainbow(5);
-        printf("hallo");
-    }
-    delete n;
+
 
     /*ol test;
     std::cout << "Test" << std::endl;
