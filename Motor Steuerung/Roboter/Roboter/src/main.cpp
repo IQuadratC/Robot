0b1ff0c66d94e5f6a81538dad24ad6d15847f9f1
Timplayer <tim.jae@gmx.de>
2021-07-09 14:57:02 +0200

    led update

--- Motor Steuerung/Roboter/Roboter/src/main.cpp
+++ Motor Steuerung/Roboter/Roboter/src/main.cpp
@@ -1,13 +1,36 @@
 #include"utility/Vector2.h"
 #include"Roboter/Roboter.h"
 #include"Lidar/Lidar.h"
+#include"Led/LedStripe.h"
 #include"iostream"
 #include<cstdio>
+#include<chrono>
 
 int main(int argc, char** argv) {
 
 
 
+
+    LedStripe test;
+    test.setLed(0, 255, 0, 0);
+    int index = 60;
+    
+    while (true)
+    {
+        int blue = 0;
+        for (int i = 1; i < index; i++)
+        {
+            test.setLed(i, 10, 0, blue);
+            test.setLed(i - 1,0, 0, 0);
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+            blue++;
+        }
+        index--;
+        std::cout << "test" << std::endl;
+    }
+        
+ 
+    std::cin.get();
     /*ol test;
     std::cout << "Test" << std::endl;
     startserial(test);
