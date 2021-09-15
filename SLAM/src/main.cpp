2e7a19feefc171c942ac98e64da0b34cf0180857
Stroby241 <46872913+Stroby241@users.noreply.github.com>
2021-09-15 19:46:12 +0200

    Function from paper

--- SLAM/src/main.cpp
+++ SLAM/src/main.cpp
@@ -1,9 +1,61 @@
 
 #include <glm/glm.hpp>
 #include <cmath>	
-inline int index(int x,int y) { return x + (10 * y);}
 
-float data[100];
+#define MAPSIZE 10
+#define POINTSSIZE 10
+
+inline int index(int x,int y, int size) { return x + (size * y);}
+
+float mapData[MAPSIZE * MAPSIZE];
+
+glm::vec2 points[POINTSSIZE];
+
+
+float func4(glm::vec2 m){
+
+    float x = m.x;
+    float y = m.y;
+    
+    int x0 = (int) x;
+    int y0 = (int) y;
+    int x1 = x0 + 1;
+    int y1 = y0 + 1;
+    
+    float dx0 = (x - x0) / (x1 - x0);
+    float dx1 = (x1 - x) / (x1 - x0);
+    float dy0 = (y - y0) / (y1 - y0);
+    float dy1 = (y1 - y) / (y1 - y0);
+    
+    float a = dy0 * (dx0 * mapData[index(x1, y1, MAPSIZE)] + dx1 * mapData[index(x0, y1, MAPSIZE)]) +
+            dy1 * (dx0 * mapData[index(x1, y0, MAPSIZE)] + dx1 * mapData[index(x0, y0, MAPSIZE)]);
+
+    return a;
+}
+
+glm::vec2 func56(glm::vec2 m){
+    
+    float x = m.x;
+    float y = m.y;
+    
+    int x0 = (int) m.x;
+    int y0 = (int) m.y;
+    int x1 = x0 + 1;
+    int y1 = y0 + 1;
+    
+    float dx0 = (x - x0) / (x1 - x0);
+    float dx1 = (x1 - x) / (x1 - x0);
+    float dy0 = (y - y0) / (y1 - y0);
+    float dy1 = (y1 - y) / (y1 - y0);
+
+    float ax = dy0 * (mapData[index(x1, y1, MAPSIZE)] - mapData[index(x0, y1, MAPSIZE)]) + 
+            dy1 * (mapData[index(x1, y0, MAPSIZE)] - mapData[index(x0, y0, MAPSIZE)]);
+
+    float ay = dx0 * (mapData[index(x1, y1, MAPSIZE)] - mapData[index(x0, y1, MAPSIZE)]) + 
+            dx1 * (mapData[index(x1, y0, MAPSIZE)] - mapData[index(x0, y0, MAPSIZE)]);
+
+    return glm::vec2(ax, ay);
+}
 
 glm::vec2 func8(glm::vec3 E, glm::vec2 S){
     
@@ -17,63 +69,43 @@ glm::vec2 func8(glm::vec3 E, glm::vec2 S){
     return matrix * S + temp;
 }
 
-float func4(glm::vec2 m){
+glm::mat2x3 func14(glm::vec3 E, glm::vec2 S){
+    glm::mat2x3 matrix;
+    matrix[0][0] = 1;
+    matrix[0][1] = 0;
+    matrix[0][2] = -(sin(E.z) * S.x) - (cos(E.z) * S.y);
+    matrix[1][0] = 0;
+    matrix[1][1] = 1;
+    matrix[1][2] = (cos(E.z) * S.x) - (sin(E.z) * S.y);
 
-    float x = m.x;
-    float y = m.y;
-    
-    int x0 = (int) m.x;
-    int y0 = (int) m.y;
-    int x1 = x0 + 1;
-    int y1 = y0 + 1;
-    
-    float dx0 = (x - x0) / (x1 - x0);
-    float dx2 = (x1 - x) / (x1 - x0);
-    float dy1 = (y - y0) / (y1 - y0);
-    float dy2 = (y1 - y) / (y1 - y0);
-    
-    float a = dy1 * (dx0 * data[index(x1, y1)] + dx1 * data[index(x0, y1)]) +
-            dy2 * (dx0 * data[index(x0, y1)] + dx1 * data[index(x0, y0)]);
-
-    return a;
+    return matrix;
 }
 
-glm::vec2 func56(){
-    
-    float x = m.x;
-    float y = m.y;
-    
-    int x0 = (int) m.x;
-    int y0 = (int) m.y;
-    int x1 = x0 + 1;
-    int y1 = y0 + 1;
-    
-    float dx0 = (x - x0) / (x1 - x0);
-    float dx2 = (x1 - x) / (x1 - x0);
-    float dy1 = (y - y0) / (y1 - y0);
-    float dy2 = (y1 - y) / (y1 - y0);
+glm::vec3 func12(glm::vec3 E){
+    for (size_t i = 0; i < POINTSSIZE; i++)
+    {
+        glm::vec2 pos = func8(E, points[i]);
 
-    float ax = dx0 * (data[index(x1, y1)] - data[index(x0, y1)]) + 
-            dx1 * (data[index(x1, y0)] - data[index(x0, y0)]);
+        glm::mat2x3 mat0 = func56(pos) * func14(E, points[i]);
+        glm::mat3x2 mat1 = glm::transpose(mat0);
 
-    float ay = dy0 * (data[index(x1, y1)] - data[index(x0, y1)]) + 
-            dy1 * (data[index(x1, y0)] - data[index(x0, y0)]);
-
-    return glm::vec2(ax, ay);
+    }
+    
 }
-
-
-
 
 int main(){
 
-    
+    mapData[index(0,0, MAPSIZE)] = 0;
+    mapData[index(0,1, MAPSIZE)] = 1;
+    mapData[index(1,0, MAPSIZE)] = 0;
+    mapData[index(1,1, MAPSIZE)] = 0;
 
-    glm::mat4 test1;
-    glm::vec3 test2;
+    func4(glm::vec2(0.5, 0.5));
+    func56(glm::vec2(0, 0.5));
 
+    func14(glm::vec3(0,0,0),  glm::vec2(1,1));
 
-
+    func12(glm::vec3(0.5,0.5,1));
 
     return 0;
 }
